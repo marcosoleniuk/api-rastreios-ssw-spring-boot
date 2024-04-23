4422e421e17d4d0b7ca4c9ed5c622d484c3b9858
MarcosOleniuk <marcos@moleniuk.com>
2024-04-22 22:50:18 -0300

    add cache

--- src/main/java/com/moleniuk/apirastreiossw/services/RastreioSSWServices.java
+++ src/main/java/com/moleniuk/apirastreiossw/services/RastreioSSWServices.java
@@ -9,6 +9,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -23,6 +24,7 @@ public class RastreioSSWServices {
     private final ObjectMapper objectMapper;
     private final ExtrairURLFromOnclick extrairURLFromOnclick;
 
+    @Cacheable("rastreioCache")
     public String rastrear(String cpf) {
         var urlFull = "https://ssw.inf.br";
         String url = urlFull + "/2/resultSSW_dest?cnpjdest=" + cpf;
@@ -38,36 +40,39 @@ public class RastreioSSWServices {
 
             Document docDetalhado = Jsoup.connect(urlFull + urlDetalhado).get();
 
-            String destinatario = Objects.requireNonNull(Objects.requireNonNull(docDetalhado.select("td:contains(Destinatário)").first()).nextElementSibling()).text();
-
-            Elements linhasRastreamento = docDetalhado.select("tr[class^=background]");
-            List<Rastreamento> rastreamentoList = new ArrayList<>();
-            for (Element linha : linhasRastreamento) {
-                String dataHora = linha.select("td").get(0).select("p").text().replace("<br>", " ");
-                String localUnidade = linha.select("td").get(1).select("p").text().replace("<br>", " ");
-                String situacao = linha.select("td").get(2).select("p").text();
-
-                Rastreamento rastreamento = new Rastreamento(dataHora, localUnidade, situacao);
-                rastreamentoList.add(rastreamento);
-            }
-
-            Elements rows = Objects.requireNonNull(docDetalhado.select("table").first()).select("tr");
-            String nColeta = rows.get(3).select("td").get(1).text().trim();
-            String nNotaFiscal = rows.get(3).select("td").get(3).text().trim();
-            String nPedido = rows.get(3).select("td").get(5).text().trim();
-
-            ObjetoRastreamento objetoRastreamento = new ObjetoRastreamento();
-            objetoRastreamento.setDestinatario(destinatario.isEmpty() ? null : destinatario );
-            objetoRastreamento.setColeta(nColeta.isEmpty() ? null : nColeta );
-            objetoRastreamento.setNotaFiscal(nNotaFiscal.isEmpty() ? null : nNotaFiscal );
-            objetoRastreamento.setPedido(nPedido.isEmpty() ? null : nPedido );
-            objetoRastreamento.setRastreamento(rastreamentoList);
-
+            ObjetoRastreamento objetoRastreamento = extrairDadosRastreamento(docDetalhado);
             return objectMapper.writeValueAsString(objetoRastreamento);
 
         } catch (IOException e) {
-            e.printStackTrace();
             return "{\"message\": \"Erro ao tentar acessar a página.\"}";
         }
+    }
+
+    private ObjetoRastreamento extrairDadosRastreamento(Document docDetalhado) {
+        String destinatario = Objects.requireNonNull(Objects.requireNonNull(docDetalhado.select("td:contains(Destinatário)").first()).nextElementSibling()).text();
+        Elements linhasRastreamento = docDetalhado.select("tr[class^=background]");
+        List<Rastreamento> rastreamentoList = new ArrayList<>();
+        for (Element linha : linhasRastreamento) {
+            String dataHora = linha.select("td").get(0).select("p").text().replace("<br>", " ");
+            String localUnidade = linha.select("td").get(1).select("p").text().replace("<br>", " ");
+            String situacao = linha.select("td").get(2).select("p").text();
+
+            Rastreamento rastreamento = new Rastreamento(dataHora, localUnidade, situacao);
+            rastreamentoList.add(rastreamento);
+        }
+
+        Elements rows = Objects.requireNonNull(docDetalhado.select("table").first()).select("tr");
+        String nColeta = rows.get(3).select("td").get(1).text().trim();
+        String nNotaFiscal = rows.get(3).select("td").get(3).text().trim();
+        String nPedido = rows.get(3).select("td").get(5).text().trim();
+
+        ObjetoRastreamento objetoRastreamento = new ObjetoRastreamento();
+        objetoRastreamento.setDestinatario(destinatario.isEmpty() ? null : destinatario);
+        objetoRastreamento.setColeta(nColeta.isEmpty() ? null : nColeta);
+        objetoRastreamento.setNotaFiscal(nNotaFiscal.isEmpty() ? null : nNotaFiscal);
+        objetoRastreamento.setPedido(nPedido.isEmpty() ? null : nPedido);
+        objetoRastreamento.setRastreamento(rastreamentoList);
+
+        return objetoRastreamento;
     }
 }
